26a8e5c1fa3883b685ef44719fe8294bfdedeced
Owcollier <owcollier@gmail.com>
2017-12-21 15:04:18 -0500

    chugging away with the tests

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import Feedback from './feedback';
+import {Feedback} from './feedback';
 
 describe('<Feedback />', () => {
     it('Renders without crashing', () => {
@@ -11,7 +11,8 @@ describe('<Feedback />', () => {
 
 it('Renders some feedback', () => {
     let TEST_FEEDBACK = 'You are listening to a game!';
+    let TEST_GUESS_COUNT = 4;
 
-    let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
-    expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+    let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} guessCount={TEST_GUESS_COUNT} />);
+    expect(wrapper.contains(TEST_FEEDBACK, TEST_GUESS_COUNT)).toEqual(true);
 });
--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 
-import GuessForm from './guess-form';
+import {GuessForm} from './guess-form';
+
+import {makeGuess} from '../actions';
 
 describe('<GuessForm />', () => {
   it('Renders without crashing', () => {
     shallow(<GuessForm />);
   });
 
-  it('Should fire the onMakeGuess callback when the form is submitted', () => {
-    const callback = jest.fn();
-    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+  it('Should dispatch the makeGuess action when the form is submitted', () => {
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm dispatch={dispatch}/>);
     const value = 10;
     wrapper.find('input[type="number"]').instance().value = value;
     wrapper.simulate('submit');
-    expect(callback).toHaveBeenCalledWith(value.toString());
+    expect(dispatch).toHaveBeenCalledWith(makeGuess(value.toString()));
   });
 
   it('Should reset the input when the form is submitted', () => {
-    const wrapper = mount(<GuessForm />);
+    const dispatch = jest.fn();
+    const wrapper = mount(<GuessForm dispatch={dispatch}/>);
     const input = wrapper.find('input[type="number"]');
     input.instance().value = 10;
     wrapper.simulate('submit');
